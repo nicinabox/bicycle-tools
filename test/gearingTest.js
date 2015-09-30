cf52d8561eed6facbd41389e3b649ee51c56ba41
Nic Aitch <nic@nicinabox.com>
2015-09-30 16:29:59 -0500

    Update gearing test

--- test/gearingTest.js
+++ test/gearingTest.js
@@ -4,13 +4,15 @@ import gainRatio from '../src/gainRatio'
 import gearInches from '../src/gearInches'
 import development from '../src/development'
 import skidPatches from '../src/skidPatches'
+import speed from '../src/speed'
 
 describe('46/16 gearing', () => {
   var gearing = {
     chainring: 46,
     cog: 16,
     crank: 165,
-    wheel: 622 // ISO
+    wheel: 622, // ISO
+    cadence: 90,
   }
 
   it('gear ratio should be 2.88', () => {
@@ -35,5 +37,69 @@ describe('46/16 gearing', () => {
 
   it('ambidextrous skid patches should be 16', () => {
     expect(skidPatches(gearing, true), 'to equal', 16)
+  })
+
+  it('speed should be 8.43m/s', () => {
+    expect(speed(gearing), 'to equal', 8.43)
+  })
+})
+
+describe('43/15 gearing', () => {
+  var gearing = {
+    chainring: 43,
+    cog: 15,
+    crank: 170,
+    wheel: 622, // ISO
+    cadence: 90,
+  }
+
+  it('gear ratio should be 2.87', () => {
+    expect(gearRatio(gearing), 'to be', 2.87)
+  })
+
+  it('gain ratio should be 5.24', () => {
+    expect(gainRatio(gearing), 'to equal', 5.24)
+  })
+
+  it('gear inches should be 70in', () => {
+    expect(gearInches(gearing), 'to equal', 70)
+  })
+
+  it('development should be 5.6m', () => {
+    expect(development(gearing), 'to equal', 5.6)
+  })
+
+  it('skid patches should be 15', () => {
+    expect(skidPatches(gearing), 'to equal', 15)
+  })
+
+  it('ambidextrous skid patches should be 30', () => {
+    expect(skidPatches(gearing, true), 'to equal', 30)
+  })
+
+  it('speed should be 8.4m/s', () => {
+    expect(speed(gearing), 'to equal', 8.4)
+  })
+})
+
+describe('48/12 gearing', () => {
+  var gearing = {
+    chainring: 48,
+    cog: 12,
+    crank: 170,
+    wheel: 622, // ISO
+    cadence: 90,
+  }
+
+  it('skid patches should be 1', () => {
+    expect(skidPatches(gearing), 'to equal', 1)
+  })
+
+  it('ambidextrous skid patches should be 1', () => {
+    expect(skidPatches(gearing, true), 'to equal', 1)
+  })
+
+  it('speed should be 11.73m/s', () => {
+    expect(speed(gearing), 'to equal', 11.73)
   })
 })
