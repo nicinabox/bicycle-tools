1fe71f603023ef5a77010c142eba997124e89be2
Nic Aitch <nic@nicinabox.com>
2015-10-01 09:14:23 -0500

    Update module test

--- test/moduleTest.js
+++ test/moduleTest.js
@@ -1,27 +1,25 @@
 import expect from 'unexpected'
 
 describe('module', () => {
-  describe('formulas', () => {
-    it('requires formulas', () => {
-      var tools = require('../index.js')
-      expect(tools.formulas, 'to have keys', [
-        'development',
-        'gainRatio',
-        'gearInches',
-        'gearRatio',
-        'skidpatches',
-        'speed'
-      ])
-    })
+  it('requires formulas', () => {
+    var tools = require('../index.js')
+    expect(tools.formulas, 'to have keys', [
+      'development',
+      'gainRatio',
+      'gearInches',
+      'gearRatio',
+      'skidpatches',
+      'speed'
+    ])
+  })
 
-    it('requires sizes', () => {
-      var tools = require('../index.js')
-      expect(tools.sizes, 'to have keys', [
-        'chainrings',
-        'cogs',
-        'cranks',
-        'wheels',
-      ])
-    })
+  it('requires sizes', () => {
+    var tools = require('../index.js')
+    expect(tools.sizes, 'to have keys', [
+      'chainrings',
+      'cogs',
+      'cranks',
+      'wheels',
+    ])
   })
 })
